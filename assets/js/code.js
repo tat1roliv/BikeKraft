895ae1303dce01c8e5dcd144dd4261fb863a56ef
tati oliveira <tat1roliv@gmail.com>
2021-12-15 10:17:57 -0300

    js

---
+++ assets/js/code.js
@@ -0,0 +1,8 @@
+
+new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
