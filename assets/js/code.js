781ce4f65b08e338ec7af956975685fe58ea28de
tati oliveira <tat1roliv@gmail.com>
2021-12-16 20:08:19 -0300

    simple slide js

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,7 +1,7 @@
 
 new SimpleSlide({
     slide: "quote", // nome do atributo data-slide="principal"
-    nav: true, // se deve ou não mostrar a navegação
+    /*nav: true, // se deve ou não mostrar a navegação*/
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
     pauseOnHover: true, // pausa a transição automática
